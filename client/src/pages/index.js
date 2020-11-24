5358353b02fe4e39297db0c8fb710cf6ff1cc685
Nick Borisenko <nborisenko55@gmail.com>
2020-11-24 14:06:35 -0600

    index page explanation started

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,6 +4,9 @@ import Navbar from '../components/layout/Navbar';
 import { useAuth } from '../context/auth';
 import Router from 'next/router'
 
+import {faCaretDown,faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export default function Home() {
   const { user, loading } = useAuth()
    // if logged in, redirect to the dashboard
@@ -15,8 +18,8 @@ export default function Home() {
 
   return (
     <Fragment>
-      <Navbar />
-      <section className='flex items-center justify-center h-screen overlay landing'>
+      <div className="landing"> <Navbar />
+      <section id='home' className='flex items-center justify-center'>
         <div className='dark-overlay'>
           <div className='text-white text-center'>
             <h1 className=' text-6xl m-5'>531 Tracker</h1>
@@ -37,6 +40,21 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <div className='text-center object-bottom'>
+        <FontAwesomeIcon icon={faCaretDown} size='4x' id='down-arrow'className='animate-pulse text-teal-200 object-bottom'/>
+        </div>
+      <section id='about'  className='flex items-center'>
+      <div className='dark-overlay text-center'>
+      <h1 className=' text-6xl m-5 pt-10'>Want to know more?</h1>
+          <div className='text-white m-2 '>
+            <div>Welcome to my rendition of the 5/3/1 workout program by Jim Wendler. Once you log in you will be prompted to create a profile with your
+              one rep max's for each lift, if you don't know them, not to worry you can estimate with the calculator. Then you will have access to your dashboard,
+              your progress chart and view the stats of your co-users.
+            </div>        
+          </div>
+        </div>
+      </section></div>
+     
     </Fragment>
   );
 }
