4a6a24e1894b51c5861dcae861d36067a2d0abca
Nick Borisenko <nborisenko55@gmail.com>
2021-02-03 22:50:18 -0600

    trying rewrite function

--- client/next.config.js
+++ client/next.config.js
@@ -2,4 +2,12 @@ module.exports = {
     env: {
       URL: process.env.URL,
     },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://531-tracker-next-js-9a2xjvfgk.vercel.app/:path*',
+        },
+      ]
+    },
   };
